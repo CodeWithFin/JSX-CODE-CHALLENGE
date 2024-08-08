0890528ee423d615ec43f810cc6d48c40072e0aa
Finley <finley.mwachia12@gmail.com>
2024-08-08 12:12:29 +0300

    add files for project

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
